ea9b5e1702227d9a22bd6d90125aaf740f611178
Arjun Komath <arjun@hey.com>
2025-01-12 17:22:17 +1100

    Enhance Capture class with fetch methods

--- src/index.ts
+++ src/index.ts
@@ -1,94 +1,127 @@
-/**
- * @summary Capture Node SDK
- * @author Arjun Komath <arjunkomath@gmail.com>
- */
-
-import qs from "qs";
 import md5 from "md5";
+import qs from "qs";
 
 export type RequestType = "image" | "pdf" | "content" | "metadata";
 export type RequestOptions = Record<string, string | number | boolean>;
 
 export class Capture {
-  static API_URL = "https://cdn.capture.techulus.in";
+	static API_URL = "https://cdn.capture.techulus.in";
 
-  key: string;
-  secret: string;
+	key: string;
+	secret: string;
 
-  constructor(key: string, secret: string) {
-    this.key = key;
-    this.secret = secret;
-  }
+	constructor(key: string, secret: string) {
+		this.key = key;
+		this.secret = secret;
+	}
 
-  private _generateToken(secret: string, url: string) {
-    return md5(secret + url);
-  }
+	private _generateToken(secret: string, url: string) {
+		return md5(secret + url);
+	}
 
-  private _toQueryString(options: RequestOptions) {
-    const filterFunc = (key: string, value: string | number) => {
-      if (key === "format") {
-        return;
-      }
-      if (!value) {
-        return;
-      }
-      return value;
-    };
+	private _toQueryString(options: RequestOptions) {
+		const filterFunc = (key: string, value: string | number) => {
+			if (key === "format") {
+				return;
+			}
+			if (!value) {
+				return;
+			}
+			return value;
+		};
 
-    const fixedEncodeURIComponent = (str: string) =>
-      encodeURIComponent(str).replace(
-        /[!'()*]/g,
-        (c) => "%" + c.charCodeAt(0).toString(16)
-      );
+		const fixedEncodeURIComponent = (str: string) =>
+			encodeURIComponent(str).replace(
+				/[!'()*]/g,
+				(c) => `%${c.charCodeAt(0).toString(16)}`,
+			);
 
-    return qs.stringify(options, {
-      encoder: fixedEncodeURIComponent,
-      filter: filterFunc,
-      arrayFormat: "repeat",
-    });
-  }
+		return qs.stringify(options, {
+			encoder: fixedEncodeURIComponent,
+			filter: filterFunc,
+			arrayFormat: "repeat",
+		});
+	}
 
-  private _buildUrl(type: RequestType, url: string, options?: RequestOptions) {
-    if (!this.key || !this.secret) {
-      throw new Error("Key and Secret is required");
-    }
-    if (typeof url !== "string") {
-      throw new Error(
-        "url should be of type string (something like www.google.com)"
-      );
-    }
-    if (url === null) {
-      throw new Error("url is required");
-    }
-    let query = "";
-    if (options) {
-      options = Object.assign({}, options, {
-        url: url,
-      });
-      query = this._toQueryString(options);
-    } else {
-      options = {
-        url: url,
-      };
-      query = this._toQueryString(options);
-    }
-    const token = this._generateToken(this.secret, query);
-    return `${Capture.API_URL}/${this.key}/${token}/${type}?${query}`;
-  }
+	private _buildUrl(
+		type: RequestType,
+		url: string,
+		requestOptions?: RequestOptions,
+	) {
+		if (!this.key || !this.secret) {
+			throw new Error("Key and Secret is required");
+		}
+		if (typeof url !== "string") {
+			throw new Error(
+				"url should be of type string (something like www.google.com)",
+			);
+		}
+		if (url === null) {
+			throw new Error("url is required");
+		}
 
-  buildImageUrl(url: string, options?: RequestOptions) {
-    return this._buildUrl("image", url, options);
-  }
+		let query = "";
+		let options = Object.assign({}, requestOptions);
+		if (options) {
+			options = Object.assign({}, options, {
+				url: url,
+			});
+			query = this._toQueryString(options);
+		} else {
+			options = {
+				url: url,
+			};
+			query = this._toQueryString(options);
+		}
+		const token = this._generateToken(this.secret, query);
+		return `${Capture.API_URL}/${this.key}/${token}/${type}?${query}`;
+	}
 
-  buildPdfUrl(url: string, options?: RequestOptions) {
-    return this._buildUrl("pdf", url, options);
-  }
+	buildImageUrl(url: string, options?: RequestOptions) {
+		return this._buildUrl("image", url, options);
+	}
 
-  buildContentUrl(url: string, options?: RequestOptions) {
-    return this._buildUrl("content", url, options);
-  }
+	buildPdfUrl(url: string, options?: RequestOptions) {
+		return this._buildUrl("pdf", url, options);
+	}
 
-  buildMetadataUrl(url: string, options?: RequestOptions) {
-    return this._buildUrl("metadata", url, options);
-  }
+	buildContentUrl(url: string, options?: RequestOptions) {
+		return this._buildUrl("content", url, options);
+	}
+
+	buildMetadataUrl(url: string, options?: RequestOptions) {
+		return this._buildUrl("metadata", url, options);
+	}
+
+	async fetchImage(
+		url: string,
+		options?: RequestOptions,
+	): Promise<ArrayBuffer> {
+		return fetch(this.buildImageUrl(url, options)).then((res) =>
+			res.arrayBuffer(),
+		);
+	}
+
+	async fetchPdf(url: string, options?: RequestOptions): Promise<ArrayBuffer> {
+		return fetch(this.buildPdfUrl(url, options)).then((res) =>
+			res.arrayBuffer(),
+		);
+	}
+
+	async fetchContent(
+		url: string,
+		options?: RequestOptions,
+	): Promise<{ success: boolean; html: string; textContent: string }> {
+		return fetch(this.buildContentUrl(url, options)).then((res) => res.json());
+	}
+
+	async fetchMetadata(
+		url: string,
+		options?: RequestOptions,
+	): Promise<{
+		success: boolean;
+		metadata: Record<string, string | number>;
+	}> {
+		return fetch(this.buildMetadataUrl(url, options)).then((res) => res.json());
+	}
 }
